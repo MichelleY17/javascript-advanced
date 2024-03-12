4333b9c6a96675805ad72df511712b7538d05760
MichelleY17 <yanezdayanna47@gmail.com>
2024-03-12 13:27:05 +0100

    hbj

--- fizzbuzz/main.js
+++ fizzbuzz/main.js
@@ -1,7 +1,19 @@
-const N= 15;
-const fizz= 3;
+const N = 15;
+const fizz = 3;
 const buzz = 5;
-console.log('!');
+
+
+
+
 for(let i=1 ; i<=N;i++){
-    console.log('...........................que haces???')
-}
+    let output="";
+    if(i%3===0){
+        output="fizz"
+    }
+    if(i%5===0){
+        output="buzz"
+    }
+    console.log(i)
+};
+
+
