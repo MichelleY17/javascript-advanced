df74c62f95bc73b0298c9d1ed518d83df952f8b2
MichelleY17 <yanezdayanna47@gmail.com>
2024-03-12 11:58:04 +0100

    passa

--- swapi-try/swapi-filter/main.js
+++ swapi-try/swapi-filter/main.js
@@ -5,16 +5,32 @@ import { filterList } from './modules/utils.js';
 // fetch dei film e composizione delle <option> della <select>
 const films = await fetchAllFilms();
 const filmSelect = document.getElementById('filmSelect');
+// filmSelect.addEventListener('change',function(event){
+  
+// })
+
+/*   for (const f of films){
+    const opt = document.createElement('option');
+    opt.value = f.episode_number;
+    opt.textContent = f.title;
+    filmSelect.appendChild(opt); 
+  } */
 films.forEach(f => {
+  
   const opt = document.createElement('option');
   opt.value = f.episode_number;
   opt.textContent = f.title;
 
   filmSelect.appendChild(opt);
+  // per appendChild(dom nod)(segnala errore aiuta a debuggare) e append(no dom nod) sono diverse append funziona come push
 });
 
-// fetch dei personaggi e composizione della lista nell'<aside>
+
+
 let people = await fetchAllPeople();
+// fetch dei personaggi e composizione della lista nell'<aside>
+
+
 const peopleWithHtml = [];
 
 for (const person of people) {
@@ -31,30 +47,34 @@ for (const person of people) {
     list.querySelectorAll('a').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 
-    // render
+ 
     renderCard(personWithHtml);
+       // render!
   });
 
   peopleWithHtml.push(personWithHtml)
 }
 
-// set degli ascoltatori su <input> e <select> rispettivamente
+
 let filmFilter = '';
 let nameFilter = '';
+let filteredList = peopleWithHtml;
+// set degli ascoltatori su <input> e <select> rispettivamente
 
-filmSelect.addEventListener('change', (e) => {
-  filmFilter = e.target.value;
-  const filteredList = filterList(peopleWithHtml, nameFilter, filmFilter);
+
+filmSelect.addEventListener('change', function(event) {
+  filmFilter = event.target.value;
+  const filteredList = peopleWithHtml.filter(p=>p.films.map(film[film.length -2]).includes(filmFilter));
+  console.log(filteredList)
   renderList(filteredList.map(({ html }) => html));
 })
 
 const nameInput = document.getElementById('nameInput');
 nameInput.addEventListener('keyup', (e) => {
   nameFilter = e.target.value.toLowerCase();
-  const filteredList = filterList(peopleWithHtml, nameFilter, filmFilter);
-  renderList(filteredList.map(el => el.html))
+  const filteredList = peopleWithHtml.filter(p=>p.name.toLowerCase().includes(nameFilter));
+  renderList(filteredList.map(f => f.html))
 });
 
 renderList(peopleWithHtml.map(({ html }) => html));
-// renderCard(peopleWithHtml[0]);
 
