d6e871981782de5f6cbb5c946184ee87564f0116
MichelleY17 <yanezdayanna47@gmail.com>
2024-03-15 15:43:33 +0100

    try-01

--- fizzbuzz/main.js
+++ fizzbuzz/main.js
@@ -2,19 +2,23 @@ const N = 15;
 const fizz = 3;
 const buzz = 5;
 
+const outputContainer = document.getElementById('fizzbuzz-container');
 
-
-
-for(let i=1 ; i<=N;i++){
-    let output="";
-    if(i%3===0){
-        output="fizz"
-    }else if(i%5===0){
-        output="buzz"
-    }else{
-        output=i;
+for(let i = 1; i <= N; i++) {
+    let output = "";
+    if (i % fizz === 0 && i % buzz === 0) {
+        output = "FizzBuzz";
+    } else if (i % fizz === 0) {
+        output = "Fizz";
+    } else if (i % buzz === 0) {
+        output = "Buzz";
+    } else {
+        output = i;
     }
-    console.log(output)
-};
+    const outputElement = document.createElement('div');
+    outputElement.textContent = output;
+    outputContainer.appendChild(outputElement);
+}
+
 
 
