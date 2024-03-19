d75258100c23dce6feeddb848057d4a4e0bbd249
MichelleY17 <yanezdayanna47@gmail.com>
2024-03-19 12:10:36 +0100

    almostthere01

--- fizzbuzz/main.js
+++ fizzbuzz/main.js
@@ -1,25 +1,47 @@
-const N = 15;
-const fizz = 3;
-const buzz = 5;
-
 const fizzbuzzContainer = document.getElementById('fizzbuzz-box');
 const btn = document.getElementById('btn');
+const btnReturn= document.getElementById('btn-return');
+const gameRulesContainer = document.querySelector('.game-rules');
 
-for(let i = 1; i <= N; i++) {
-    let output = "";
-    if (i % fizz === 0 && i % buzz === 0) {
-        output = "FizzBuzz";
-    } else if (i % fizz === 0) {
-        output = "Fizz";
-    } else if (i % buzz === 0) {
-        output = "Buzz";
+btn.addEventListener('click', ()=> {
+  gameRulesContainer.style.display = 'none';
+  fizzbuzzContainer.style.display = 'grid';
+  
+  const maxNumber = parseInt(document.querySelector('input[name="number-max"]').value);
+  const n1 = parseInt(document.querySelector('input[name="number-fizz"]').value);
+  const n2 = parseInt(document.querySelector('input[name="number-buzz"]').value);
+  const fizzBuzzResults = fizzBuzzFunction(maxNumber, n1, n2);
+  displayFizzBuzzResults(fizzBuzzResults);
+});
+btnReturn.addEventListener('click', ()=>{
+  gameRulesContainer.style.display = 'block';
+  fizzbuzzContainer.style.display = 'none';
+});
+
+const fizzBuzzFunction = (maxNumber, n1, n2, fizzWord = 'Fizz', buzzWord = 'Buzz') => {
+  const output = [];
+  for (let i = 1; i <= maxNumber; i++) {
+    if (i % n1 === 0 && i % n2 === 0) {
+      output.push(fizzWord + buzzWord);
+    } else if (i % n1 === 0) {
+      output.push(fizzWord);
+    } else if (i % n2 === 0) {
+      output.push(buzzWord);
     } else {
-        output = i;
+      output.push(i);
     }
-    const outputElement = document.createElement('div');
-    outputElement.textContent = output;
-    fizzbuzzContainer.appendChild(outputElement);
-}
+  }
+  return output;
+};
+
+const displayFizzBuzzResults = (results) => {
+  const fizzbuzzContainer = document.getElementById('fizzbuzz-box');  
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.textContent = result;
+    fizzbuzzContainer.appendChild(div);
+  });
+};
 
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
@@ -29,12 +51,10 @@ function getRandomColor() {
   }
   return color;
 }
-
 function changeBackgroundColor() {
   fizzbuzzContainer.style.backgroundColor = getRandomColor();
 }
 
-btn.addEventListener('click', changeBackgroundColor);
 
 
 
